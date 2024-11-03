8c23ed7fc0696211f4cbd7a9511be0c868ca2151
Sarthak Roy <sarthakroy2002@gmail.com>
2024-11-03 18:15:15 +0530

    cancunf: Use `cancunf` as DeviceName prop key
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Change-Id: I096016396e1a9b5fda3903deab5841a1b90acd8d
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- yaap_cancunf.mk
+++ yaap_cancunf.mk
@@ -24,6 +24,6 @@ PRODUCT_MODEL := moto g54 5G
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    DeviceName=cancunf_g_sys \
+    DeviceName=cancunf \
     BuildDesc="cancunf_g_sys-user 14 U1TDS34.94-12-9-10-1 2d53a-f2829 release-keys" \
     BuildFingerprint=motorola/cancunf_g_sys/cancunf:14/U1TDS34.94-12-9-10-1/2d53a-f2829:user/release-keys
