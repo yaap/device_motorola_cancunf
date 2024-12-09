529988a980316f4abde48143a9fe35ee617f0454
Sarthak Roy <sarthakroy2002@gmail.com>
2024-12-09 19:11:21 +0530

    cancunf: Build android.hardware.sensors@2.1-service.cancunf-multihal
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Change-Id: I4285f710baa3848fc41624402021e3cd1efbe9ef
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- device.mk
+++ device.mk
@@ -381,7 +381,7 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.0-service.multihal
+    android.hardware.sensors@2.1-service.cancunf-multihal
 
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(DEVICE_PATH)/configs/sensors/,$(TARGET_COPY_OUT_VENDOR)/etc/sensors)
