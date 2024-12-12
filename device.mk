61783ef2cda14e516f3e8bbc50ef6b9e505f1e79
Alcatraz323 <alcatraz32323@gmail.com>
2024-12-12 21:07:07 +0800

    cancunf: Implement VolumeSynchronizer
    * This is needed to synchronize volume with framework
      to limit bass when the volume is high to prevent
      distortion.
    
    [sarthakroy2002: Rebrand DSPVolumeSynchronizer to VolumeSynchronizer]
    Change-Id: I7e06f8667f8abc84b97190c53567dd41e38c6833
    
    Change-Id: I2ec9c79d8ba6f7100bdb2714c84fc3c8c7494d1e

--- device.mk
+++ device.mk
@@ -76,6 +76,9 @@ PRODUCT_PACKAGES += \
     DolbyManager \
     MtkInCallService
 
+PRODUCT_PACKAGES += \
+    VolumeSynchronizer
+
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/audio/audio_device.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_device.xml \
     $(DEVICE_PATH)/configs/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
