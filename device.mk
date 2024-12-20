ff022732402696c4fb68786a9b741bcafb03545f
Alexander Koskovich <akoskovich@pm.me>
2024-12-20 20:28:16 +0530

    cancunf: sensors: Add hals.conf as a required prebuilt
    Change-Id: I634ac114824c992736a30053aa3f3e05da5da330
    Signed-off-by: LinkBoi00 <linkdevel@protonmail.com>
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>

--- device.mk
+++ device.mk
@@ -379,9 +379,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.sensors@2.1-service.cancunf-multihal
 
-PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*,$(DEVICE_PATH)/configs/sensors/,$(TARGET_COPY_OUT_VENDOR)/etc/sensors)
-
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 33
 
