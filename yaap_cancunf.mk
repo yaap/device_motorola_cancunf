fd715b14e727ed695ddefdfa2726644e103bcce9
Sarthak Roy <sarthakroy2002@gmail.com>
2025-02-05 18:36:36 +0530

    cancunf: Update to `U1TDS34.94-12-9-10-2`
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Change-Id: Ia24cf67d8f25b4cfd010e7d05500cadb469dcdeb

--- yaap_cancunf.mk
+++ yaap_cancunf.mk
@@ -25,5 +25,5 @@ PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     DeviceName=cancunf \
-    BuildDesc="cancunf_g_sys-user 14 U1TDS34.94-12-9-10-1 2d53a-f2829 release-keys" \
-    BuildFingerprint=motorola/cancunf_g_sys/cancunf:14/U1TDS34.94-12-9-10-1/2d53a-f2829:user/release-keys
+    BuildDesc="cancunf_g_sys-user 14 U1TDS34.94-12-9-10-2 e34746-5853b release-keys" \
+    BuildFingerprint=motorola/cancunf_g_sys/cancunf:14/U1TDS34.94-12-9-10-2/e34746-5853b:user/release-keys
