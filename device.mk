f67530608591da56ac661848dd3352f7aff9a588
Mashopy <eliasgheeraert@gmail.com>
2026-01-30 14:26:52 +0000

    cancunf: Build MediaTek WiFi lib
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Change-Id: I8ebcd3c75c582125beb4a1ba78b83fe875984904
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- device.mk
+++ device.mk
@@ -419,6 +419,7 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/google/pixel \
     hardware/mediatek \
     hardware/mediatek/libmtkperf_client \
+    hardware/mediatek/wlan/wifi_hal \
     $(DEVICE_PATH)
 
 # Thermal
@@ -456,9 +457,6 @@ PRODUCT_PACKAGES += \
     hostapd \
     wpa_supplicant \
     android.hardware.wifi.supplicant@1.4.vendor
-
-PRODUCT_PACKAGES += \
-    libwifi-hal-wrapper
 
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(DEVICE_PATH)/configs/wifi/,$(TARGET_COPY_OUT_VENDOR)/etc/wifi)
